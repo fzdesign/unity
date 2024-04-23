f2ad301323ab364a4bd01b949fb3c066889ff0df
fzdesign <zhudesign12@gmail.com>
2024-04-23 12:28:20 -0400

    Timer Added

--- Cmail_app/Assets/Scripts/Swipe.cs
+++ Cmail_app/Assets/Scripts/Swipe.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 
 public class Swipe : MonoBehaviour
 {
-    
+
+    public Text countdown;
+    float timer = 30;
+    float timeleft;
+
     void Start()
     {
         
@@ -14,6 +18,12 @@ public class Swipe : MonoBehaviour
     
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        countdown.text = timer.ToString();
+
+       // if (timer - Time.deltaTime == 0)
+       // {
+        //    Application.Quit();
+        //}
     }
 }
