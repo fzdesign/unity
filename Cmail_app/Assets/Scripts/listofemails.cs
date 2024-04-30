d20df1e1cb006672821be50224ed5c14e3b6b76e
fzdesign <zhudesign12@gmail.com>
2024-04-30 08:02:58 -0400

    Final Build
    Everything worked.

--- Cmail_app/Assets/Scripts/listofemails.cs
+++ Cmail_app/Assets/Scripts/listofemails.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class listofemails : MonoBehaviour
 {
@@ -11,10 +12,26 @@ public class listofemails : MonoBehaviour
     public int itemsCount = 50; 
     private bool isLoadingMoreItems = false; 
     public float threshold = 100f; 
-    private int totalItems = 0; 
+    private int totalItems = 0;
+    private Vector2 startPos;
+    private bool isSwiping = false;
+    public float swipeThreshold = 50f;
+    public GameObject button;
+    public GameObject UiList;
+    public RectTransform panelTransform;
+    public float speed = 1.0f;
+    public Text emailcount;
+    int email;
+    public AudioClip swipeSound;
+    private AudioSource audioSource;
+    public string SceneName;
+
+
+
     void Start()
     {
         LoadItems(itemsCount);
+
     }
 
     void Update()
@@ -23,7 +40,56 @@ public class listofemails : MonoBehaviour
         {
             LoadItems(itemsCount);
         }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+
+            // Detect touch start
+            if (touch.phase == TouchPhase.Began)
+            {
+                startPos = touch.position;
+                isSwiping = true;
+            }
+
+            // Detect touch end
+            if (touch.phase == TouchPhase.Ended)
+            {
+                isSwiping = false;
+                Vector2 endPos = touch.position;
+                Vector2 swipeDirection = endPos - startPos;
+
+                if (Mathf.Abs(swipeDirection.x) > swipeThreshold && swipeDirection.x < 0)
+                {
+
+                    DestroyFirstChild();
+                    float moveAmount = speed * Time.deltaTime;
+                    //panelTransform.anchoredPosition += new Vector2(0f, 300);
+                    UiList.GetComponent<RectTransform>().anchoredPosition += new Vector2(0f, 100);
+                    //email -= 1;
+                    emailcount.text = email.ToString();
+                    if (email == 0)
+                    {
+                        SceneManager.LoadScene("Ending");
+                        Debug.Log("win the game");
+                    }
+
+                }
+            }
+        }
     }
+
+    private void DestroyFirstChild()
+    {
+        if (UiList.transform.childCount > 0)
+        {
+            Destroy(UiList.transform.GetChild(0).gameObject);
+            audioSource.Play();
+        }
+    }
+
+
 
     private bool ShouldLoadMoreItems()
     {
--- Cmail_app/Assets/Scripts/newswipe.cs
+++ Cmail_app/Assets/Scripts/newswipe.cs
@@ -2,38 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class newswipe : MonoBehaviour
 {
 
     private Vector2 startPos;
     private bool isSwiping = false;
-    public float swipeThreshold = 50f; // Adjust this value according to your needs
+    public float swipeThreshold = 50f; 
     public GameObject button;
     public GameObject UiList;
     public RectTransform panelTransform; 
     public float speed = 1.0f;
     public Text emailcount;
-    int email = 50;
- 
+    int email;
+    public AudioClip swipeSound; 
+    private AudioSource audioSource;
+    public string SceneName;
 
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
-        
+        email = UiList.transform.childCount;
 
-
-        // Check for touch input
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-
-            // Detect touch start
-            if (touch.phase == TouchPhase.Began)
-            {
-                startPos = touch.position;
-                isSwiping = true;
-            }
+           
+            
+                // Detect touch start
+                if (touch.phase == TouchPhase.Began)
+                {
+                    startPos = touch.position;
+                    isSwiping = true;
+                }
 
             // Detect touch end
             if (touch.phase == TouchPhase.Ended)
@@ -43,22 +49,31 @@ public class newswipe : MonoBehaviour
                 Vector2 swipeDirection = endPos - startPos;
 
                 if (Mathf.Abs(swipeDirection.x) > swipeThreshold && swipeDirection.x < 0)
-                {   
-                    
-                    Destroy(button);
+                {
+
+                    DestroyFirstChild();
                     float moveAmount = speed * Time.deltaTime;
-                    panelTransform.anchoredPosition += new Vector2(0f, 200);
-                    UiList.GetComponent<RectTransform>().anchoredPosition += new Vector2(0f,20);
+                    //panelTransform.anchoredPosition += new Vector2(0f, 100);
+                    UiList.GetComponent<RectTransform>().anchoredPosition += new Vector2(0f, 50);
                     email -= 1;
                     emailcount.text = email.ToString();
                     if (email == 0)
                     {
-
+                        SceneManager.LoadScene("Ending");
                         Debug.Log("win the game");
                     }
 
                 }
             }
+        }
+    }
+
+    private void DestroyFirstChild()
+    {
+        if (UiList.transform.childCount > 0)
+        {
+            Destroy(UiList.transform.GetChild(0).gameObject);
+            audioSource.Play();
         }
     }
 }
